4ad511b2f087701784371dd7dc55725a4d349ecd
NomadicDaggy <dagsagrinbergs@gmail.com>
2024-05-17 23:20:31 +0300

    spawn multiple enemies at random locations

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -6,7 +6,7 @@ var enemy_scene = preload("res://scenes/enemy.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -15,12 +15,20 @@ func _process(delta):
 
 
 func _on_map_map_ready():
-	spawn_enemies()
+	var rng = RandomNumberGenerator.new()
+	var r = 40
+	for i in 15:
+		var x = rng.randi_range(-r,r)
+		var y = rng.randi_range(-r,r)
+		if (x > -10 and x < 10) or (y > -10 and y < 10):
+			continue
+		spawn_enemy(x, y)
 
 
-func spawn_enemies():
+func spawn_enemy(tile_pos_x: int, tile_pos_y: int):
 	var enemy = enemy_scene.instantiate()
-	enemy.position += Vector2(3.0 * 16.0, 5.0 * 16.0)
+	enemy.position += Vector2(tile_pos_x * 16.0, tile_pos_y * 16.0)
+	# TODO: check if enemy has been spawned within a wall
 	enemy.target = player
 	enemy.z_index = 500
 	add_child(enemy)
--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -3,6 +3,11 @@ extends Area2D
 @onready var timer = $Timer
 
 func _on_body_entered(body):
+	
+	# Got stuck in wall or smth
+	if body.name != "Player":
+		return
+	
 	print("You died!")
 	Engine.time_scale = 0.1
 	body.get_node("CollisionShape2D").queue_free()
